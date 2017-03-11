ee85d5d7fa99489fb28c85a50822f1948213710d
smithrm941 <smithrm941@gmail.com>
2017-03-10 17:54:59 -0800

    finished 'query the data' 09, 11, 13. Started on 12

--- sql/09-mutual-likes.sql
+++ sql/09-mutual-likes.sql
@@ -2,12 +2,7 @@
 of both students. Include each pair only once, with the two names in alphabetical
 order.*/
 
-/*Created liker_info and likee_info tables in 08-query-likes-grade-two-or-more.sql
-liker_info has these columns: liker_id, liker_name, liker_grade, likee_id
-likee_info has these columns: likee_id, likee_name, likee_grade, liker_id*/
-
-/*Used these tables to create table of pairs of student likers and likees*/
-
+/*I used the tables I created in 08-query-likes-grade-two-or-more.sql to create table of pairs of student likers and likees*/
 CREATE TABLE student_info.likers_likees AS
   SELECT student_info.liker_info.liker_id, student_info.liker_info.liker_name, student_info.liker_info.liker_grade,
   student_info.likee_info.likee_id, student_info.likee_info.likee_name, student_info.likee_info.likee_grade
@@ -15,8 +10,10 @@ CREATE TABLE student_info.likers_likees AS
   JOIN student_info.likee_info
   ON student_info.liker_info.liker_id = student_info.likee_info.liker_id;
 
-/*Concatenated the couples together*/
-SELECT
-  CONCAT (liker_id, ' ',liker_name,' ',liker_grade,' and ', likee_id, ' ', likee_name,' ',likee_grade)
-FROM
-  student_info.likers_likees;
+/*Used a self join to find the pairs of students who mutually like each other*/
+SELECT a.liker_name, a.liker_grade, b.liker_name, b.liker_grade
+FROM student_info.likers_likees a, student_info.likers_likees b
+WHERE a.liker_id = b.likee_id
+AND a.likee_id = b.liker_id
+AND a.liker_id < b.liker_id
+ORDER BY a.liker_name;
--- sql/12-find-friends-in-common.sql
+++ sql/12-find-friends-in-common.sql
@@ -1,3 +1,28 @@
 /*For each student A who likes a student B where the two are not friends,
 find if they have a friend C in common (who can introduce them!). For all such
 trios, return the name and grade of A, B, and C.*/
+
+/*Used table I created in 09-mutual likes and the friend table from 02-create-table-friend to find students A and B*/
+CREATE TABLE student_info.stranger_crushes AS
+  SELECT student_info.likers_likees.liker_id,student_info.likers_likees.liker_name, student_info.likers_likees.liker_grade,
+  student_info.likers_likees.likee_id,
+  student_info.likers_likees.likee_name,
+  student_info.likers_likees.likee_grade
+    FROM student_info.likers_likees
+      WHERE student_info.likers_likees.likee_id NOT IN (SELECT student_info.friend.id2 FROM student_info.friend WHERE student_info.friend.id1 = liker_id)
+      AND student_info.likers_likees.likee_id NOT IN (SELECT student_info.friend.id1 FROM student_info.friend WHERE student_info.friend.id2 = liker_id);
+
+/*Added and id for each friend pair in friend table - might not be needed
+ALTER TABLE student_info.friend ADD COLUMN id SERIAL PRIMARY KEY;*/
+
+/*gives liker name, likee name and friend ids of every friend pairing involving liker*/
+  SELECT student_info.stranger_crushes.liker_id, student_info.stranger_crushes.liker_name, student_info.stranger_crushes.likee_name, student_info.friend.id1, student_info.friend.id2
+    FROM student_info.friend, student_info.stranger_crushes
+    WHERE (student_info.stranger_crushes.liker_id = student_info.friend.id1
+    OR student_info.stranger_crushes.liker_id = student_info.friend.id2);
+
+/*gives liker name, likee name and friend ids of every friend pairing involving likee*/
+  SELECT student_info.stranger_crushes.likee_id, student_info.stranger_crushes.likee_name, student_info.stranger_crushes.liker_name, student_info.friend.id1, student_info.friend.id2
+      FROM student_info.friend, student_info.stranger_crushes
+      WHERE (student_info.stranger_crushes.likee_id = student_info.friend.id1
+      OR student_info.stranger_crushes.likee_id = student_info.friend.id2);
